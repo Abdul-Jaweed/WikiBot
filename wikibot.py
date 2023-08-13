ee8c1eecb2301efa0cc319eaed56da36faafe8c8
Abdul Jaweed <114526738+Abdul-Jaweed@users.noreply.github.com>
2023-08-13 22:24:20 +0000

    nothing

--- wikibot.py
+++ wikibot.py
@@ -7,3 +7,18 @@ def scrape(name="Microsoft", length=10):
 
 
 print(scrape("Wikipedia"))
+
+
+# import click
+# from mylib.bot import scrape
+
+
+# @click.command()
+# @click.option("--name", help="Web page we want to scrape")
+# def cli(name):
+#     result = scrape(name)
+#     click.echo(click.style(f"{result}:", fg="blue"))
+
+
+# if __name__ == "__main__":
+#     cli()
